740633883514d6ba280d4c5c624ab5a111bbe951
markthree <1801982702@qq.com>
2023-07-13 14:19:41 +0800

    feat: add cors

--- mod.ts
+++ mod.ts
@@ -1,11 +1,20 @@
-import { Hono } from 'https://deno.land/x/hono@v3.3.0/mod.ts'
-import { serveStatic } from 'https://deno.land/x/hono@v3.3.0/middleware.ts'
+import { Hono } from "https://deno.land/x/hono@v3.3.0/mod.ts";
+import {
+  cors,
+  serveStatic,
+} from "https://deno.land/x/hono@v3.3.0/middleware.ts";
 
-const app = new Hono()
-
-app.get('/', (c) => c.text('Hello Deno!'))
+const app = new Hono();
 
 // 静态服务
-app.use('/static/*', serveStatic({ root: './' }))
+app.use("/static/*", serveStatic({ root: "./" }));
 
-Deno.serve(app.fetch)
+// 跨域
+app.use(
+  "/api/*",
+  cors({
+    origin: [], // 配置需要跨的域
+  }),
+);
+
+Deno.serve(app.fetch);
