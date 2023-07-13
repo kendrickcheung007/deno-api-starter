9e57b5e469669f1efedcad123e4fb0cc1412b0b5
markthree <1801982702@qq.com>
2023-07-13 15:50:16 +0800

    feat: add logger

--- mod.ts
+++ mod.ts
@@ -1,10 +1,22 @@
+import { consoleDateLog, writeDateLog } from "./utils/log.ts";
 import { Hono } from "https://deno.land/x/hono@v3.3.0/mod.ts";
 import {
   cors,
+  logger,
   serveStatic,
 } from "https://deno.land/x/hono@v3.3.0/middleware.ts";
 
 const app = new Hono();
+
+// 日志
+app.use(
+  "*",
+  logger((text) => {
+    const date = new Date();
+    consoleDateLog(text, date)
+    writeDateLog(text, date)
+  }),
+);
 
 // 静态服务
 app.use("/static/*", serveStatic({ root: "./" }));
